51f46b49499dfbdf809b4a2069b9c1acbe5eb8b9
Rishi Raj <rishiraj@Rishis-MacBook-Air.local>
2025-03-23 21:13:46 +0530

    server updatess

--- server.js
+++ server.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB Connected'))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log('MongoDB Connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
 
